faa98bc7c7d7ee040d42dc7809f4ab104190aa18
Pasindu Dilshan <always.pasindu@gmail.com>
2024-08-05 12:32:02 +0530

    Fixed contents in homepage

--- script.js
+++ script.js
@@ -34,3 +34,30 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
+
+
+   // =====================Scroll reveal===========================//
+
+   ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay:200
+ });
+
+ ScrollReveal().reveal(' .home-content, .heading', { origin: 'top' });
+ ScrollReveal().reveal(' .home-img, .services-container, .portfolio-box, contact form', { origin: 'bottom' });
+ ScrollReveal().reveal(' .home-content h1, .about-img', { origin: 'left' });
+ ScrollReveal().reveal(' .home-content p, .about-content', { origin: 'right' });
+
+
+    // =====================typed js===========================//
+
+    const typed = new Typed('.multiple-text', {
+        strings: ['Software Engineer', 'Full-Stack Developer', 'Tech Enthusiast', 'YouTuber', 'Content Creator'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+
+    });
